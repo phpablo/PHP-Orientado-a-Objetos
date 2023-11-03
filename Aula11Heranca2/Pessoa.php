97d61a33c99949c5d12b24ac6b3fd8b1fbd74769
phpablo <pablohenrique120@gmail.com>
2023-11-03 10:38:17 -0300

    :memo: Cria Classe Pessoa e Visitante

---
+++ Aula11Heranca2/Pessoa.php
@@ -0,0 +1,39 @@
+<?
+abstract class Pessoa
+{
+  private $nome;
+  private $idade;
+  private $sexo;
+
+
+  public final function fazerNiver()
+  {
+    $this->idade++;
+  }
+
+  function getNome()
+  {
+    return $this->nome;
+  }
+  function getIdade()
+  {
+    return $this->idade;
+  }
+  function getSexo()
+  {
+    return $this->sexo;
+  }
+
+  function setNome($nome)
+  {
+    $this->nome = $nome;
+  }
+  function setIdade($idade)
+  {
+    $this->idade = $idade;
+  }
+  function setSexo($sexo)
+  {
+    $this->sexo = $sexo;
+  }
+}
--- Aula11Heranca2/index.php
+++ Aula11Heranca2/index.php
@@ -8,7 +8,18 @@
 </head>
 
 <body>
+  <pre>
+    <?php
+    require_once 'Visitante.php';
+    // $p1 = new Pessoa(); Dá erro pois um classe abstrata ñ se pd instanciar objetos dela 
+    $v1 = new Visitante();
+    $v1->setNome('Goku');
+    $v1->setIdade(12);
+    $v1->setSexo('M');
 
+    print_r($v1);
+    ?>
+  </pre>
 
 </body>
 
